92308ef63ac695a0958b08766b249fcd8f225337
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-17 08:50:09 +0500

    learning

--- ch-7.c
+++ ch-7.c
@@ -7,17 +7,23 @@ int main()
     float temp;
 
     printf("\nIs the temperature in (F) or (C)?: ");
-    scanf("%c", &unit);
+    scanf(" %c", &unit);
 
     unit = toupper(unit);
 
-    if (unit == "C")
+    if (unit == 'C')
     {
-        printf("\nthe temp is currently in C");
+        printf("\nEnter the temp in Celsius:");
+        scanf("%f", &temp);
+        temp = (temp * 9 / 5) + 32;
+        printf("\nThe temp in Farenheit is: %.1f", temp);
     }
-    else if (unit == "F")
+    else if (unit == 'F')
     {
-        printf("The temp is currently in F");
+        printf("\nEnter the temp in Farenheit:");
+        scanf("%f", &temp);
+        temp = ((temp - 32) * 5) / 9;
+        printf("\nThe temp in Celsius is: %.1f", temp);
     }
     else
     {
