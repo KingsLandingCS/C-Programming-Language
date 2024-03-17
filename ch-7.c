440967997a28dcb4e0b880c9b59c1d8a79153e8e
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-17 07:19:17 +0500

    learning

---
+++ ch-7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char unit;
+    float temp;
+
+    printf("\nIs the temperature in (F) or (C)?: ");
+    scanf("%c", &unit);
+
+    unit = toupper(unit);
+
+    if (unit == "C")
+    {
+        printf("\nthe temp is currently in C");
+    }
+    else if (unit == "F")
+    {
+        printf("The temp is currently in F");
+    }
+    else
+    {
+        printf("\n %c is not a valid unit of measurement", unit);
+    }
+
+    return 0;
+}
