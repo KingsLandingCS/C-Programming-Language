8d404b1bf65d40128933432c9f3680ae4d383d3d
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-08 04:47:14 +0500

    learning

--- quiz-game-app/quiz-game.c
+++ quiz-game-app/quiz-game.c
@@ -5,7 +5,7 @@ int main()
     int i;
     int ans1, ans2, ans3, ans4, ans5;
     int point1, point2, point3, point4, point5, point6, point7, point8, point9, point10;
-    int point01, point02, point02, point03, point04, point05, point06, point07, point08, point09, point010;
+    int point01, point02, point03, point04, point05, point06, point07, point08, point09, point010;
     int total1, total2, total3;
 
     printf("Welcome to the Game\n\n");
@@ -54,7 +54,7 @@ int main()
         }
 
         printf("2) Which is the first web browser developed in 1990? \n\n");
-        printf("1) Internet Explorer\n\n");
+        printf("1) Internet Explorer\n");
         printf("2) Mosaic\n");
         printf("3) Mozilla\n");
         printf("4) Nexus\n");
@@ -78,7 +78,68 @@ int main()
         printf("3) First computer virus is know as?\n\n");
         printf("1) Rabbit\n");
         printf("2) Creeper Virus\n");
-        printf("3) Elk Cloner");
+        printf("3) Elk Cloner\n");
         printf("4) SCA Virus\n");
+
+        printf("Enter your answer: ");
+        scanf("%d", &ans3);
+
+        if (ans3 == 2)
+        {
+            printf("Corrent Answer\n");
+            point3 = 5;
+            printf("You have scored %d point\n", point3);
+        }
+        else
+        {
+            printf("Wrong Answer\n");
+            point03 = 0;
+            printf("You have scored %d point\n", point03);
+        }
+
+        printf("4) Firewall in computer is used for?\n\n");
+        printf("1) Security\n");
+        printf("2) Data Transmission\n");
+        printf("3 Monitoring\n");
+        printf("4) Authentication\n");
+
+        printf("Enter your answer : ");
+        scanf("%d", &ans4);
+
+        if (ans4 == 1)
+        {
+            printf("Correct Answer\n");
+            point4 = 5;
+            printf("You have scored %d point\n", point4);
+        }
+        else
+        {
+            printf("Wrong Answer\n");
+            point04 = 0;
+            printf("You have scored %d point\n", point04);
+        }
+        printf("5) which of the following is not a database management software?\n\n");
+        printf("1) Mysql\n");
+        printf("2) Oracle\n");
+        printf("3) Cobol\n");
+        printf("4) Sybase\n");
+
+        printf("Enter your answer : ");
+        scanf("%d", &ans5);
+
+        if (ans5 == 3)
+        {
+            printf("Correct Answer\n");
+            point5 = 5;
+            printf("You have scored %d point\n", point5);
+        }
+        else
+        {
+            printf("Wrong Answer\n");
+            point05 == 0;
+            printf("You have scored %d point\n", point05);
+        }
     }
+
+    return 0;
 }
