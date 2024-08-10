a740b129e8b47f134a5a78237b9179406024c35c
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-10 05:43:02 +0500

    learning

--- the-c-programming-language/minimum-number-array.c
+++ the-c-programming-language/minimum-number-array.c
@@ -2,18 +2,28 @@
 
 int main(void)
 {
-    int myarray[] = {5, 9, 10, 11, 4, 1, 8, 6, 7};
+    int myarray1[] = {5, 9, 10, 11, 4, 1, 8, 6, 7};
+    int myarray2[] = {2, 5, 6, 3, 7, 10, 0};
+
+    int min1 = find_min(myarray1, 9);
+    int min2 = find_min(myarray2, 7);
+    printf("min1: %d\n", min1);
+    printf("min2: %d\n", min2);
+    return 0;
+}
+
+int find_min(int array[], int length)
+{
     int min;
-    min = myarray[0];
-    for (int i = 0; i < 9; i++)
+    min = array[0];
+    for (int i = 0; i < length; i++)
     {
-        printf("Checking myarray[%d] = %d\n", i, myarray[i]);
-        if (myarray[i] < min)
+        printf("Checking myarray[%d] = %d\n", i, array[i]);
+        if (array[i] < min)
         {
-            min = myarray[i];
+            min = array[i];
             printf("New min found: %d\n", min);
         }
     }
-    printf("Minimum: %d\n", min);
-    return 0;
+    return min;
 }
