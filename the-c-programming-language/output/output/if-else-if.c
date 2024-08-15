7cf40de451408ca07c93e1f7af401ce263acdf3f
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-15 10:47:30 +0500

    learning

---
+++ the-c-programming-language/output/output/if-else-if.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int grade = 0;
+    printf("Grade: ");
+    scanf("%d", &grade);
+
+    if (grade >= 90)
+    {
+        printf("Congrats you got grade A!\n");
+    }
+    else if (grade >= 80)
+    {
+        printf("Congrats you got grade B!\n");
+    }
+    else if (grade >= 70)
+    {
+        printf("Congrats you got grade C!\n");
+    }
+    else if (grade >= 60)
+    {
+        printf("Congrats you got grade D!\n");
+    }
+    else if (grade >= 50)
+    {
+        printf("Congrats you got grade F!\n");
+    }
+    else
+    {
+        printf("Better luck next time!\n ");
+    }
+
+    return 0;
+}
