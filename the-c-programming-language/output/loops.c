12721e6677b241223560f639f3d10e399a17bfab
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-20 13:45:42 +0500

    learning

---
+++ the-c-programming-language/output/loops.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    int number = 0;
+    int total = 0;
+    while (i < 5)
+    {
+        printf("Enter Number %d: ", i + 1);
+        scanf("%d", &number);
+        total = total + number;
+        i++;
+    }
+    printf("total: %d\n", total);
+    printf("average: %d\n", total / 5);
+    return 0;
+}
