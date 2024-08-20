0a0c722d713311e747feb23211c56e41e35b49da
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-20 14:06:15 +0500

    learning

---
+++ the-c-programming-language/output/output/output/while-loop-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number = 0;
+    int max = -1;
+
+    while (number != -1)
+    {
+        printf("Enter a number: ");
+        scanf("%d", &number);
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+
+    printf("max: %d\n", max);
+    return 0;
+}
