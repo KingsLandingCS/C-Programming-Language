4656ad37045f263e70296161bc8d104d98e8eb3f
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-24 04:43:11 +0500

    learning

--- the-c-programming-language/output/output/output/output/output/output/for-loop-2.c
+++ the-c-programming-language/output/output/output/output/output/output/for-loop-2.c
@@ -2,10 +2,47 @@
 
 int main(void)
 {
-    int i = 0;
-    for (i = 0; i < 10; i++)
+    double initial = 0, step = 0, stop = 0;
+
+    do
     {
-        printf("i: %d\n", i);
+        printf("Initial (m): ");
+        scanf("%lf", &initial);
+        if (initial < 0)
+        {
+            printf("Initial Must be greater than or equal to 0!\n");
+        }
+    } while (initial < 0);
+
+    do
+    {
+        printf("Step (m): ");
+
+        scanf("%lf", &step);
+        if (step <= 0)
+        {
+            printf("Step Must be greater than 0!\n");
+        }
+    } while (initial <= 0);
+
+    do
+    {
+        printf("Stop (m): ");
+
+        scanf("%lf", &stop);
+        if (stop < 0)
+        {
+            printf("Stop Must be greater than or equal to 0!\n");
+        }
+    } while (stop < 0);
+
+    printf("\n");
+    printf("Meters    Feet");
+    printf("************************\n");
+    for (double conv = initial; conv <= stop; conv += step)
+    {
+        printf("%f %f\n", conv, conv * 3.28084); // 1 m = 3.28084 ft
     }
+
     return 0;
 }
