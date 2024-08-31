8f2b79161b7bb9ba7d9b374c5bb019dd34263b65
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-31 10:53:42 +0500

    learning

--- the-c-programming-language/arrays.c
+++ the-c-programming-language/arrays.c
@@ -8,15 +8,18 @@
 
 int main(void)
 {
-    int grade[5];
-
-    grade[0] = 92;
-    grade[1] = 85;
-    grade[2] = 72;
-    grade[3] = 73;
-    grade[4] = 95;
+    int grade[] = {92, 85, 72, 73, 95};
 
     for (int i = 0; i < 5; i++)
+    {
         printf("grade[%d]=%d\n", i, grade[i]);
+    }
+
+    int total = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        total += grade[i];
+    }
+    printf("average: %d\n", total / 5);
     return 0;
 }
