bb77e16d58370ea05119d9a978e0a14b419e9f53
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-09-03 04:30:08 +0500

    learning

---
+++ the-c-programming-language/flipping-case.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char s[] = "This Is tHe Way.";
+    printf("s: %s\n", s);
+    int length = strlen(s);
+    for (int i = 0; i < length; i++)
+    {
+        if (isupper(s[i]))
+        {
+            s[i] = tolower(s[i]);
+        }
+        else if (islower(s[i]))
+        {
+            s[i] = toupper(s[i]);
+        }
+    }
+    printf("s: %s\n", s);
+
+    return 0;
+}
