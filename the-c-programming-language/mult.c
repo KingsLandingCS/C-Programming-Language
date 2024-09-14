60c730f1618a09c906d4571e2491b01759be0e34
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-09-14 08:38:09 +0500

    learning

---
+++ the-c-programming-language/mult.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+double mult(double x, double y);
+double power(double x, int n);
+
+int main(void)
+{
+    printf("%f\n", power(2, 3));
+    printf("%f\n", power(2, 1));
+    printf("%f\n", power(5.8, 9));
+
+    return 0;
+}
+
+double mult(double x, double y)
+{
+    return x * y;
+}
+
+double power(double x, int n)
+{
+    double result = x;
+    for (int i = 1; i < n; i++)
+    {
+        result = mult(result, x);
+    }
+    return result;
+}
