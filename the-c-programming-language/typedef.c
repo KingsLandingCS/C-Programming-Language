f3741f36b381068ff8a078ce64d96e436b468907
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-18 08:32:22 +0500

    learning

---
+++ the-c-programming-language/typedef.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+typedef int inches;
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+int main(void)
+{
+    struct Point p1;
+    p1.x = 5;
+    p1.y = 3;
+    printf("(%d, %d)\n", p1.x, p1.y);
+
+    struct Point p2;
+
+    p2.x = 10;
+    p2.y = 4;
+
+    printf("(%d, %d)\n", p2.x, p2.y);
+
+    struct Point p3 = {1, 2};
+
+    printf("(%d, %d)\n", p3.x, p3.y);
+
+    struct Point p4 =
+        {
+            .y = 7, .x = 6};
+
+    return 0;
+}
